8bbdc1c9ff3d0907bd26d68f327bc614d3b3ab8a
Serena Berry <siberry226@gmail.com>
2019-04-21 12:22:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ document.querySelector("main#main").remove();
 
 let newHeader = document.createElement("h1");
 newHeader.id = "victory"
-newHeader.id = "${name} is the champion"
+newHeader.id = "is the champion"
